eba7fb5eaf59251b5c446461d9d1d89dde3b1d11
Jherico3217 <detorresjherico84@gmail.com>
2023-10-28 12:06:46 +0800

    feat insert data syntax

--- hero.sql
+++ hero.sql
@@ -68,3 +68,22 @@ CREATE TABLE IF NOT EXISTS public.HeroItem
     FOREIGN KEY (hero_id) REFERENCES Hero(hero_id),
     FOREIGN KEY (item_id) REFERENCES Item(item_id)
 );
+
+CREATE DATABASE hero001;
+
+--2.	Create new branch named "feat/insert-data"
+
+-- Insert data into the Class table
+INSERT INTO Class (class_id, class_name, class_description)
+VALUES
+    (101, 'Warrior', 'A strong and melee-focused class'),
+    (102, 'Mage', 'A magic-wielding class'),
+    (103, 'Rogue', 'A stealthy and agile class'),
+    (104, 'Skilled Archers', 'Precise ranged attackers'),
+    (105, 'Range Archers', 'Skilled archers'),
+    (106, 'Assassin', 'Stealthy and high burst damage'),
+    (107, 'Fighter', 'Versatile combatants'),
+    (108, 'Support', 'Provide assistance and healing'),
+    (109, 'Tank', 'High durability and crowd control'),
+    (110, 'Wizard', 'Masters of arcane magic'); 
+
